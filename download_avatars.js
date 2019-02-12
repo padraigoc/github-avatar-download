ff48f63a1dcaabcc12e542928b4abc56d8ae8bd9
Patrick O Connor <padraigoc@gmail.com>
2019-02-12 08:25:58 +0000

    updating files

--- download_avatars.js
+++ download_avatars.js
@@ -1,29 +1,29 @@
 var request = require('request');
 
+//require secret file to store private keu
 var token = require('./secrets.js');
 
 //require the file system
 var fs = require('fs');
 
-var owner = process.argv.slice(2);
-var repo = process.argv.slice(3);
-
-
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+//get input
+var repo = process.argv.slice(2)[0]; //This was returning twice, placing [0] fixed the issue
+var owner = process.argv.slice(3)[0];
 
 
 function getRepoContributors(owner, repo, cb) {
-
   //options is the header - HTTP headers allow the client and the server to pass additional information with the request or the response. A request header consists of its case-insensitive name followed by a colon ':', then by its value.
   var options = {
-    url: "https://api.github.com/repos/" + owner + "/" + repo + "/contributors",
+    url: "https://api.github.com/repos/" + owner + "/" + repo+ "/contributors",
     headers: {
       'User-Agent': 'request',
       'Authorization' : token
     }
   };
 
-  //from the required request - this is the request option where
+  //from the required request - this is the request option
   request(options, function(err, res, body) {
     var data = JSON.parse(body); //get the body of the JSON file
     cb(err, data);
@@ -32,17 +32,17 @@ function getRepoContributors(owner, repo, cb) {
 }
 
 //https://api.github.com/repos/jquery/jquery/contributors
-getRepoContributors('jquery', 'jquery', function(err, result) { //owner and //repo not working here
-
-  console.log("Errors:", err);
-
-for (var key in result) {
-  console.log("Downloading Image! - Name: " + result[key].login + "Avatar: " + result[key].avatar_url);
-  downloadImageByURL(result[key].avatar_url, './downloadedImages/' + result[key].login + '.jpg');
-  console.log("----------------------------------------------");
-}
-
-
+getRepoContributors(owner, repo, function(err, result) {
+  if(repo !== undefined && owner !== undefined){
+    for (var key in result){
+      console.log("Downloading Image! - Name: " + result[key].login + "Avatar: " + result[key].avatar_url);
+      downloadImageByURL(result[key].avatar_url, './downloadedImages/' + result[key].login + '.jpg');
+      console.log("----------------------------------------------");
+    }
+  } else {
+    console.log("No values entered - please enter values ");
+    console.log("Errors:", err);
+  }
 });
 
 
@@ -57,8 +57,7 @@ function downloadImageByURL(url, filePath) {
          // console.log('Image downloaded!');
          })
          .pipe(fs.createWriteStream(filePath))
-
-}
+       }
 
 
 
