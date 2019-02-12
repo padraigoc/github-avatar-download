df7acd5d5393f2487bebcda977e66e13ac862678
Patrick O Connor <padraigoc@gmail.com>
2019-02-12 05:56:37 +0000

    updating files

--- download_avatars.js
+++ download_avatars.js
@@ -5,13 +5,18 @@ var token = require('./secrets.js');
 //require the file system
 var fs = require('fs');
 
+var owner = process.argv.slice(2);
+var repo = process.argv.slice(3);
+
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+
+function getRepoContributors(owner, repo, cb) {
 
   //options is the header - HTTP headers allow the client and the server to pass additional information with the request or the response. A request header consists of its case-insensitive name followed by a colon ':', then by its value.
   var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    url: "https://api.github.com/repos/" + owner + "/" + repo + "/contributors",
     headers: {
       'User-Agent': 'request',
       'Authorization' : token
@@ -27,35 +32,33 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 //https://api.github.com/repos/jquery/jquery/contributors
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors('jquery', 'jquery', function(err, result) { //owner and //repo not working here
+
   console.log("Errors:", err);
 
 for (var key in result) {
-  console.log("Name: " + result[key].login + "Avatar: " + result[key].avatar_url);
+  console.log("Downloading Image! - Name: " + result[key].login + "Avatar: " + result[key].avatar_url);
+  downloadImageByURL(result[key].avatar_url, './downloadedImages/' + result[key].login + '.jpg');
+  console.log("----------------------------------------------");
 }
+
 
 });
 
 
 //function to download image
 function downloadImageByURL(url, filePath) {
-  console.log("Downloading Image...")
+  //console.log("Downloading Image...")
   request.get(url)
          .on('error', function (err){
           throw err;
          })
          .on('response', function (response) {
-          console.log('Image downloaded!');
+         // console.log('Image downloaded!');
          })
          .pipe(fs.createWriteStream(filePath))
 
-
 }
-
-//test downloading an image
-downloadImageByURL('https://avatars1.githubusercontent.com/u/43004?v=4', './downloadedImages/test.jpg');
-
-
 
 
 
